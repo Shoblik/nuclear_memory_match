671af8b68a11ee4fe5fb1b8159d754d6dd3870df
Shoblik <shoblik@yahoo.com>
2017-10-31 19:18:17 -0700

    wire frame done with a card flipping taken care of

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 $('.card').on('click', cardHandler);
+$('header').on('click', rotateBack);
 });
 
-function cardHandler() {
-    // $(this).addClass('backFlip');
-    $(this).
 
+function cardHandler() {
+    $(this).addClass('backFlip');
+}
+
+function rotateBack() {
+    $('.card').removeClass('backFlip').css('transition','1s');
 }
 
