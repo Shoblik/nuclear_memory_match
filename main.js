1fbcb23e721ccc9ab17e1dd1ff8a28168b6afa9f
Shoblik <shoblik@yahoo.com>
2017-11-01 11:13:09 -0700

    functionality has been completed

--- main.js
+++ main.js
@@ -3,10 +3,50 @@ $('.card').on('click', cardHandler);
 $('header').on('click', rotateBack);
 });
 
+var cardCount = 0;
+var first;
+var second;
+var firstCard;
+var secondCard;
 
 function cardHandler() {
-    $(this).addClass('backFlip');
+    if (cardCount === 0) {
+        firstCard = $(this);
+        first = firstCard.find('.front').attr('compare');
+        firstCard.addClass('backFlip');
+        cardCount++;
+    }
+    else if (cardCount === 1) {
+        secondCard = $(this);
+        second = secondCard.find('.front').attr('compare');
+        secondCard.addClass('backFlip');
+        cardCount++;
+        if (first === second) {
+            firstCard.css('background-color', 'lightGreen');
+            secondCard.css('background-color', 'lightGreen');
+            console.log('matched!!!!!!!!!');
+            cardCount = 0;
+        }
+        else {
+            console.log('no matches here!!!!!');
+            setTimeout(function() {
+                firstCard.removeClass('backFlip').css('transition', '1.2s');
+                secondCard.removeClass('backFlip').css('transition', '1.2s');
+                cardCount = 0;
+            }, 1000);
+
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
 
 function rotateBack() {
     $('.card').removeClass('backFlip').css('transition','1s');
