d05991dd7728aa29170d6a94a64e266b88c6a5bb
Shoblik <shoblik@yahoo.com>
2017-11-02 14:43:19 -0700

    working on making the back of the cards getting some design in

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 $('.card').on('click', cardHandler);
-$('header').on('click', rotateBack);
+$('.reset').on('click', reset);
 });
 var totalCards = 0;
 var cardCount = 0;
@@ -13,6 +13,24 @@ var second;
 var firstCard;
 var secondCard;
 
+function reset() {
+    timesPlayed++;
+    $('.card').removeClass('backFlip').css('transition', '1s');
+    $('.card').find('.front').removeClass('foundCard').css('transition', '2s');
+    totalCards = 0;
+    cardCount = 0;
+    tryCount = 0;
+    accuracy = 0;
+    matchCount = 0;
+    first;
+    second;
+    firstCard;
+    secondCard;
+    $('.timesPlayed').text(timesPlayed);
+    $('.accuracy').text(accuracy.toFixed(1) + '%');
+    $('.tryCount').text(tryCount);
+
+}
 function cardHandler() {
     if (cardCount === 0) {
         //if it's the first card store that card and increase card count to 1
@@ -24,6 +42,7 @@ function cardHandler() {
     else if (cardCount === 1) {
         //if its the second card store that card and we'll perform a conditional check
         totalCards++;
+        $('.tryCount').text(totalCards);
         secondCard = $(this);
         second = secondCard.find('.front').attr('compare');
         secondCard.addClass('backFlip');
@@ -31,20 +50,12 @@ function cardHandler() {
         if (first === second) {
             //if the cards match things match here and card count get's reset to 0
             setTimeout(function() {
-                firstCard.find('.front').css({
-                    'background-color': 'rgba(34,139,34, .7)',
-                    'transition': '1s'
-                });
-                secondCard.find('.front').css({
-                    'background-color': 'rgba(34,139,34, .7)',
-                    'transition': '1s'
-                });
+                firstCard.find('.front').addClass('foundCard');
+                secondCard.find('.front').addClass('foundCard');
                 console.log('matched!!!!!!!!!');
-                matchCount++;
-                accuracy = (matchCount / totalCards) * 100;
-                $('.accuracy').text(accuracy.toFixed(1) + '%');
                 cardCount = 0;
             }, 500);
+            matchCount++;
             if (first === second && matchCount === 9) {
                 console.log('YOU WIN');
                 timesPlayed++;
@@ -58,13 +69,14 @@ function cardHandler() {
                 firstCard.removeClass('backFlip').css('transition', '1.0s');
                 secondCard.removeClass('backFlip').css('transition', '1.0s');
                 cardCount = 0;
-                tryCount++;
-                $('.tryCount').text(tryCount);
             }, 1000);
 
         }
 
+        accuracy = (matchCount / totalCards) * 100;
+        $('.accuracy').text(accuracy.toFixed(1) + '%');
     }
+
 }
 
 
