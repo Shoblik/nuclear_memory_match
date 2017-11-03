8943259b63bd5f4019eaf8639ef0c05602996025
Shoblik <shoblik@yahoo.com>
2017-11-03 09:42:54 -0700

    fixed a few bugs like duplicate card selection and a cheating detection feature

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-$('.card').on('click', cardHandler);
-$('.reset').on('click', reset);
+    reset();
+    $('.card').on('click', cardHandler);
+    $('.reset').on('click', reset);
+
+    /////////////////////////////////////REMOVE BEFORE RELEASE//////////////////////////
+    $('.accuracy').on('click', function() {
+       $('.card').toggleClass('backFlip');
+    });
 
 });
 var totalCards = 0;
@@ -13,9 +19,13 @@ var first;
 var second;
 var firstCard;
 var secondCard;
+var disqualCount;
+var playerOne = true;
+var compMemory = [];
 
 
 function reset() {
+    playerOne = true;
     timesPlayed++;
     $('.card').removeClass('backFlip').css('transition', '1s');
     $('.card').find('.front').removeClass('foundCard').css('transition', '2s');
@@ -24,6 +34,7 @@ function reset() {
     tryCount = 0;
     accuracy = 0;
     matchCount = 0;
+    disqualCount = 0;
     first;
     second;
     firstCard;
@@ -38,30 +49,43 @@ function reset() {
 
         var cards = $('.row').find('.card .front')
         for (var i=0;i<cards.length;i++) {
-            var randomNum = Math.floor((Math.random() * cardArr.length + 1));
+            var randomNum = Math.floor((Math.random() * cardArr.length));
             $(cards[i]).text(cardArr[randomNum]).attr('compare', cardArr[randomNum]);
             cardArr.splice(randomNum, 1);
         }
-
-
-
 }
 function cardHandler() {
     if (cardCount === 0) {
         //if it's the first card store that card and increase card count to 1
         firstCard = $(this);
+        console.log(firstCard);
         first = firstCard.find('.front').attr('compare');
         firstCard.addClass('backFlip');
         cardCount++;
+
+        //AI fuckery/////////////////////////////////////////
+        compMemory.push(first);
     }
     else if (cardCount === 1) {
         //if its the second card store that card and we'll perform a conditional check
-        totalCards++;
         $('.tryCount').text(totalCards);
         secondCard = $(this);
+        if (secondCard.hasClass('backFlip')) {
+            cardCount = 0;
+            firstCard.removeClass('backFlip').css('transition', '1.0s');
+            disqualCount++;
+            if (disqualCount == 3) {
+                reset();
+                alert('you have been disqualified for cheating!!!');
+            }
+            return;
+
+        }
         second = secondCard.find('.front').attr('compare');
         secondCard.addClass('backFlip');
+        totalCards++;
         cardCount++;
+
         if (first === second) {
             //if the cards match things match here and card count get's reset to 0
             setTimeout(function() {
@@ -95,14 +119,4 @@ function cardHandler() {
 }
 
 
-
-
-
-
-
-
-
-function rotateBack() {
-    $('.card').removeClass('backFlip').css('transition','1s');
-}
 
